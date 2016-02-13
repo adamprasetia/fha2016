ad1e421f1f12148f171a42e629e336a887ef557b
Adam Prasetia <adam.prasetia@gmail.com>
2016-02-13 16:12:47 +0700

    benerin distribution

--- application/modules/distribution/models/Distribution_model.php
+++ application/modules/distribution/models/Distribution_model.php
@@ -23,38 +23,35 @@ class Distribution_model extends CI_Model {
 	function count_by_telemarketer_new($id){
 		$this->filter();
 		$this->db->where('A.status','0');
+		$this->db->where('A.audit','0');
 		$this->db->from('candidate A');
-		$this->db->join('call_history B','A.id = B.candidate','left');
 		$this->db->where('A.telemarketer',$id);
-		$this->db->where('B.id is null');
-		$this->db->group_by('A.id');
 		return $this->db->get()->num_rows();
 	}
 	function count_by_telemarketer_onproses($id){
 		$this->filter();
-		$this->db->where('A.status','0');
+		$this->db->where('A.status <>','0');
+		$this->db->where('A.audit','0');
 		$this->db->from('candidate A');
-		$this->db->join('call_history B','A.id = B.candidate');
 		$this->db->where('A.telemarketer',$id);
-		$this->db->group_by('A.id');
 		return $this->db->get()->num_rows();
 	}
 	function count_by_telemarketer_complete($id){
 		$this->filter();
-		$this->db->where('A.status <>','0');
+		$this->db->where('A.audit','1');
 		$this->db->where('A.telemarketer',$id);
 		$this->db->from('candidate A');
 		return $this->db->get()->num_rows();
 	}
 	function clear(){
 		$this->filter();
-		$this->db->where('A.status','0');
+		$this->db->where('A.audit','0');
 		$this->db->update('candidate A',array('A.telemarketer'=>'0'));
 		return $this->db->affected_rows();
 	}
 	function clear_by_telemarketer($id){
 		$this->filter();
-		$this->db->where('A.status','0');
+		$this->db->where('A.audit','0');
 		$this->db->where('A.telemarketer',$id);
 		$this->db->update('candidate A',array('A.telemarketer'=>'0'));
 		return $this->db->affected_rows();
