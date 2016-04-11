bb0cfc5dd0edbf4760f1ea93d4a8e61397678fef
Adam Prasetia <adam.prasetia@kompas.com>
2016-04-11 10:01:30 +0700

    benerin

--- application/modules/reminder_distribution/models/Distribution_model.php
+++ application/modules/reminder_distribution/models/Distribution_model.php
@@ -60,7 +60,7 @@ class Distribution_model extends CI_Model {
 	}	
 	function clear_callback(){
 		$this->filter();
-		$this->db->where('A.status in(21,22)');
+		$this->db->where('A.status in(21,22,15)');
 		$this->db->update('reminder_candidate A',array('A.telemarketer'=>'0','A.valid'=>'0','A.audit'=>'0'));
 		return $this->db->affected_rows();
 	}	
